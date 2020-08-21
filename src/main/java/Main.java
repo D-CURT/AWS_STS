e95739f00ca78ac6db2f0b365912be5a6aa20f7e
AlexeyShevko <alexey.shevko@mbicycle.com>
2020-08-21 11:30:43 +0300

    Logger added; Credentials now are saving to a json file;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,25 @@
 import com.amazonaws.services.securitytoken.model.Credentials;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
         AWSSTSService service = new AWSSTSService();
         Credentials stsCredentials = service.getSTSCredentials("AKIAJ7DT2KMCJAFSBPIQ", "O6cyyXt3ykD5+RGcrlCg2Foo+9+UsOE1pa4NSPBJ");
-        System.out.println(String.format("accessKey: %s, secretKey: %s, sessionToken: %s",
-                stsCredentials.getAccessKeyId(),
-                stsCredentials.getSecretAccessKey(),
-                stsCredentials.getSessionToken()));
+        JSONObject result = new JSONObject()
+                .put("accessKey", stsCredentials.getAccessKeyId())
+                .put("secretKey", stsCredentials.getSecretAccessKey())
+                .put("sessionToken", stsCredentials.getSessionToken())
+                .put("expiration", stsCredentials.getExpiration());
+        log.info(result.toString());
+        Files.writeString(Path.of("result.json"), result.toString());
     }
 
 }
